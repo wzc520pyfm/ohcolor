d50e2fefadc02f8b0003052013aefc80e82268d6
wzc520pyfm <1528857653@qq.com>
2024-03-27 00:21:44 +0800

    test: add mycolor test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,33 @@
 import { expect, it, describe } from "vitest";
-import {} from "../src";
+// eslint-disable-next-line import/no-named-as-default
+import mycolor from "../src";
 
-describe("packageName", () => {
-  it.todo("pass", () => {
-    expect(true).toBe(true);
+describe("mycolor", () => {
+  it("should get mycolor by default", async () => {
+    const mycolor = await import("../src");
+    expect(mycolor).toBeDefined();
+  });
+  it("should get mycolor by named", async () => {
+    const { mycolor } = await import("../src");
+    expect(mycolor).toBeDefined();
+  });
+  it("should get a new mycolor instance", () => {
+    const color1 = mycolor(255, 165, 0, 1);
+    const color2 = mycolor(color1);
+    expect(color1).not.toBe(color2);
+  });
+  it("input rgba params, should get a rgba array", () => {
+    expect(mycolor(255, 165, 0, 1).rgba()).toEqual([255, 165, 0, 1]);
+  });
+  it("input hex, should get a rgba array", () => {
+    expect(mycolor("#ff3").rgba()).toEqual([255, 255, 51, 1]);
+    expect(mycolor("#ff36").rgba()).toEqual([255, 255, 51, 0.4]);
+    expect(mycolor("#ff3399").rgba()).toEqual([255, 51, 153, 1]);
+    expect(mycolor("#ff339966").rgba()).toEqual([255, 51, 153, 0.4]);
+    // without #
+    expect(mycolor("ff3").rgba()).toEqual([255, 255, 51, 1]);
+    expect(mycolor("ff36").rgba()).toEqual([255, 255, 51, 0.4]);
+    expect(mycolor("ff3399").rgba()).toEqual([255, 51, 153, 1]);
+    expect(mycolor("ff339966").rgba()).toEqual([255, 51, 153, 0.4]);
   });
 });
