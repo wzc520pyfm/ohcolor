d26a73dc8713243c5df3286dfb26196130e7fed0
wzc520pyfm <1528857653@qq.com>
2024-04-03 22:15:51 +0800

    feat(util): add more func for check type

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,31 @@
-const isUndefined = (s: unknown) => s === undefined;
-const isNumber = (s: unknown) => typeof s === "number";
+const TYPE_NAME = [
+  "Boolean",
+  "Number",
+  "String",
+  "Function",
+  "Array",
+  "Date",
+  "RegExp",
+  "Undefined",
+  "Null",
+];
+const classToType = Object.fromEntries(
+  TYPE_NAME.map((name) => {
+    return [`[object ${name}]`, name.toLowerCase()];
+  }),
+);
+const getType = (obj: unknown) => {
+  return classToType[Object.prototype.toString.call(obj)] || "object";
+};
+
+const isUndefined = (s: unknown) => getType(s) === "undefined";
+const isNumber = (s: unknown) => getType(s) === "number";
+const isObject = (s: unknown) => getType(s) === "object";
+const isString = (s: unknown) => getType(s) === "string";
 
 export default {
   u: isUndefined,
   n: isNumber,
+  o: isObject,
+  s: isString,
 };
