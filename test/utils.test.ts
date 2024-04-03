4928aca8f8f35a5e90dfc3b60a183df72fa27b4a
wzc520pyfm <1528857653@qq.com>
2024-04-03 23:34:19 +0800

    test: add utils test

---
+++ test/utils.test.ts
@@ -0,0 +1,18 @@
+import { expect, it, describe } from "vitest";
+import U from "../src/utils";
+
+describe("utils test", () => {
+  it("types", () => {
+    expect(U.u(undefined)).toBeTruthy();
+    expect(U.u(123)).toBeFalsy();
+
+    expect(U.n(456)).toBeTruthy();
+    expect(U.n("456")).toBeFalsy();
+
+    expect(U.o({ a: 666 })).toBeTruthy();
+    expect(U.o(789)).toBeFalsy();
+
+    expect(U.s('hello')).toBeTruthy();
+    expect(U.s(111)).toBeFalsy();
+  });
+});
