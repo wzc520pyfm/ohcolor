55c5ea2d77ee8b47b570386c6ad47d8a18de4d34
wzc520pyfm <1528857653@qq.com>
2024-04-04 19:06:43 +0800

    type(plugin): [format-hex] update return type

--- src/plugin/formatHex/type.d.ts
+++ src/plugin/formatHex/type.d.ts
@@ -1,8 +1,10 @@
+import { StringColorRGBA } from "../../type";
+
 export {};
 
 declare module "ohcolor" {
   interface MyColor {
     /** Override format with hex prop. */
-    format<T extends "hex" = "hex", R = string>(f: T): R;
+    format<T extends "hex" = "hex", R = StringColorRGBA>(f: T): R;
   }
 }
